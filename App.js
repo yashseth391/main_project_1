56f68baa540bbe08ac45c81d5168e571bf872747
yashseth391 <115554024+yashseth391@users.noreply.github.com>
2024-07-10 13:21:35 +0530

    started

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, Touchable, TouchableNativeFeedback, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  let x = 1;
+  console.log("hello");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
